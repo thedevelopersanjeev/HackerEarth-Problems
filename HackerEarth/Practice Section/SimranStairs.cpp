509fa709f9d7a2b5910fc04ef2095af855a9d55b
Sanjeev <thedevelopersanjeev@gmail.com>
2018-08-23 22:02:15 +0530

    Added a new question

---
+++ HackerEarth/Practice Section/SimranStairs.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+long func(long n)
+{
+
+	if (n == 1 || n == 0)
+
+		return 1;
+
+	else if(n == 2)
+
+		return 2;
+
+	else
+
+		return func(n-1) + func(n-2) + func(n-3);
+
+	}
+
+
+int main()
+{
+
+	ios_base::sync_with_stdio(false);
+    
+	cin.tie(NULL);
+
+	long n;
+
+	cin >>n;
+
+	cout <<func(n);
+
+	return 0;
+
+	}
