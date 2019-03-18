6851e9c0add1e1892991a9ee701b5d224f0f16de
Sanjeev Sharma <thedevelopersanjeev@gmail.com>
2019-03-18 21:51:40 +0530

    added FSQRT

---
+++ Codechef/FSQRT.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int t, n;
+	cin >>t;
+	while(t--){
+		cin >>n;
+		cout <<(int)sqrt(n) <<"\n";
+	}
+	return 0;
+}
