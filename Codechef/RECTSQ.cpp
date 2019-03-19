a8d0bd41d9ee5144b0c4bef3a0fd66a9e37dc1b1
Sanjeev Sharma <thedevelopersanjeev@gmail.com>
2019-03-19 23:42:28 +0530

    added RECTSQ

---
+++ Codechef/RECTSQ.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int t, n, m;
+	cin >>t;
+	while(t--){
+		cin >>n >>m;
+		cout <<(n*m) / (__gcd(n, m)*__gcd(n, m)) <<"\n";
+	}
+	return 0;
+}
