8e7aefdf9d7ff3e571aa1df8ea9c8905d5408b2c
Sanjeev Sharma <thedevelopersanjeev@gmail.com>
2019-03-24 07:33:39 +0530

    refactor

--- Google India Scholarship Code Challenges/GIS Code Challenge 2/DeleteDuplicates.java
+++
@@ -1,8 +0,0 @@
-static SinglyLinkedListNode removeDuplicates(SinglyLinkedListNode head) {
-        if (head == null)   return null;
-        SinglyLinkedListNode node = head;
-        while (node.next != null)
-            if(node.data == node.next.data)    node.next = node.next.next;
-            else    node = node.next;
-        return head;
-    }
