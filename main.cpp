47525ee34d783bd164c1b76fff4d5b59badf95e6
Sanjeev Sharma <thedevelopersanjeev@gmail.com>
2019-03-31 17:20:57 +0530

    minor change

--- main.cpp
+++ main.cpp
@@ -3,7 +3,9 @@
  * 
  *  @author Sanjeev
  */
-
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#pragma comment(linker, "/stack:200000000")
 #include<bits/stdc++.h>
 using namespace std;
 
