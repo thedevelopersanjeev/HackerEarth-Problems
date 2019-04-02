7ddc48e4cea4c6cd9b76dd522c5a0f129b836175
Sanjeev Sharma <thedevelopersanjeev@gmail.com>
2019-04-03 03:50:09 +0530

    added new question

--- HackerEarth/Practice Section/FrequencyOfStudents.cpp
+++ HackerEarth/Practice Section/FrequencyOfStudents.cpp
@@ -10,6 +10,16 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    
+    int t;
+    string s;
+    cin >>t;
+    map<string, int> mp;
+    while(t--){
+        cin >>s;
+        mp[s]++;
+    }
+    for(auto ele : mp){
+        cout <<ele.first <<" " <<ele.second <<"\n";
+    }
     return 0;
 }
