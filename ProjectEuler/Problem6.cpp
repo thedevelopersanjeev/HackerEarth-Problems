13eff3cd301750efbb9f67ed2b4d1cc50dafd762
Sanjeev Sharma <thedevelopersanjeev@gmail.com>
2019-04-09 17:53:55 +0530

    added new question

---
+++ ProjectEuler/Problem6.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long solve(long n){
+    return abs(pow(n*(n+1)/2, 2) - n*(n+1)*(2*n+1)/6);
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    long t, n;
+    cin >>t;
+    while(t--){
+        cin >>n;
+        cout <<solve(n) <<"\n";
+    }
+    return 0;
+}
