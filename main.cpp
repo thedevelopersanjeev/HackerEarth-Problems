a3a6b966dbf81d53aefebfb42871a097a733b58b
Sanjeev Sharma <thedevelopersanjeev@gmail.com>
2019-04-09 17:25:59 +0530

    added new question

--- main.cpp
+++ main.cpp
@@ -1,27 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool isPrime(long n){
-
-}
-
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int t;
-    long n;
-    cin >>t;
-    while(t--){
-        cin >>n;
-        long x = n - 1;
-        while(1){
-            if(n%x == 0 && isPrime(x)){
-                cout <<x <<"\n";
-                break;
-            }
-            x = x - 1;
-        }
-    }
+    
     return 0;
 }
