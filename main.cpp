badf7393074e17c53363760905fb6d2147f3339d
Sanjeev Sharma <thedevelopersanjeev@gmail.com>
2019-04-14 02:41:12 +0530

    added new question

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@
 using namespace std;
 
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     
-  return 0;
+    return 0;
 }
