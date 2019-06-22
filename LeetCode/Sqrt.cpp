8ad291d77dfa3e8cb37afd1f81624d5df740036d
Sanjeev Sharma <thedevelopersanjeev@gmail.com>
2019-06-22 14:35:07 +0530

    added sqrt

--- LeetCode/Sqrt.cpp
+++ LeetCode/Sqrt.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
-    int mySqrt(int x) {
-        return sqrt(x);
+    int mySqrt(int A) {
+    
+    if(A < 0)
+        return -1;
+        
+    if(A == 0 || A == 1)
+        return A;
+    
+    int lo = 1, hi = A;
+    
+    int mid = lo + (hi - lo) / 2;
+    
+    while(mid > A / mid || (mid + 1) <= A / (mid + 1)){
+        
+        if(mid > A / mid)
+            hi = mid;
+            
+        else
+            lo = mid;
+            
+        mid = lo + (hi - lo) / 2;
+        
+    }
+    
+    return mid;
+    
+        
     }
 };
