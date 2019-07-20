27f878a2fafc0298eabfdcffe3189c85609ac633
Sanjeev <thedevelopersanjeev@gmail.com>
2019-07-20 21:37:45 +0530

    added print all paths

--- Data Structure and Algorithms/Binary Trees/PrintAllRootToLeafPaths.cpp
+++ Data Structure and Algorithms/Binary Trees/PrintAllRootToLeafPaths.cpp
@@ -15,7 +15,40 @@ TreeNode* createNewNode(int data){
     return temp;
 }
 
-int main(){
+// Utility method to print path from root to leaf node
+// len : length of path
+// root: current node of tree
+// path: current path traversed so far
+void printPathUtil(TreeNode* root, vector<int> &path){
+    if(root == NULL){
+        path.clear();
+        return;
+    }
+    path.push_back(root->data);
+    if(root->left == NULL && root->right == NULL){
+        for(auto ele : path)
+            cout <<ele <<" ";
+        cout <<"\n";
+        path.clear();
+    }
+    else{
+        printPathUtil(root->left, path);
+        printPathUtil(root->right, path);
+    }
+}
 
+void printPath(TreeNode* root){
+    vector<int> path;
+    printPathUtil(root, path);
+}
+
+int main(){
+    TreeNode* root = createNewNode(10);
+    root->left = createNewNode(8);
+    root->right = createNewNode(2);
+    root->left->left = createNewNode(3);
+    root->left->right = createNewNode(5);
+    root->right->left = createNewNode(2);
+    printPath(root);
     return 0;
 }
