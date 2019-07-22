4401ca1c639cf9cd5179f7989160aa785651a6e4
Sanjeev <thedevelopersanjeev@gmail.com>
2019-07-22 23:23:26 +0530

    template update

--- main.cpp
+++ main.cpp
@@ -3,9 +3,21 @@
 *   Author    :    Sanjeev Sharma                   *
 *   Email     :    thedevelopersanjeev@gmail.com    *
 *****************************************************/
-
+ 
 #include <bits/stdc++.h>
 using namespace std;
+
+#define deb(x) cout <<#x <<" " <<x <<endl;
+
+template<typename... T>
+void read(T&... args){
+    ((cin >>args), ...);
+}
+
+template<typename... T>
+void write(T&&... args){
+    ((cout <<args <<" "), ...);
+}
 
 int main(){
     ios_base::sync_with_stdio(false);
