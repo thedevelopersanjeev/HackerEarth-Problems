57bc5acaa694e5d797db7334fc63c49641fb783a
Sanjeev <thedevelopersanjeev@gmail.com>
2019-07-25 03:03:12 +0530

    added min xor value

--- main.cpp
+++ main.cpp
@@ -9,16 +9,6 @@ using namespace std;
 
 #define deb(x) cout <<#x <<" " <<x <<endl;
 
-template<typename... T>
-void read(T&... args){
-    ((cin >>args), ...);
-}
-
-template<typename... T>
-void write(T&&... args){
-    ((cout <<args <<" "), ...);
-}
-
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
