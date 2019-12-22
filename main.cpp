0a45275e6194aff4d15356997575f33517aeb29c
Sanjeev <thedevelopersanjeev@gmail.com>
2019-12-23 02:12:47 +0530

    update template

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 *****************************************************/
 #pragma GCC optimize ("O3")
 #pragma GCC target ("sse4")
+#pragma GCC optimize ("unroll-loops")
+#pragma GCC optimize("no-stack-protector,fast-math")
 
 #include <bits/stdc++.h>
 #include <ext/pb_ds/tree_policy.hpp>
@@ -14,6 +16,10 @@
 const double PI = 2 * acos(0.0);
 
 using namespace std;
+using namespace __gnu_pbds;
+
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 int main(){
     ios_base::sync_with_stdio(false);
