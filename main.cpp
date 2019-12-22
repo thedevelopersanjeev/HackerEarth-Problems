18ab51a8a1b6a7e5af2e773996e83182fff7db9d
Sanjeev <thedevelopersanjeev@gmail.com>
2019-12-23 02:21:41 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,20 +4,22 @@
 *   Email     :    thedevelopersanjeev@gmail.com    *
 *****************************************************/
 #pragma GCC optimize ("O3")
-#pragma GCC target ("sse4")
+#pragma GCC optimize("Ofast")
 #pragma GCC optimize ("unroll-loops")
 #pragma GCC optimize("no-stack-protector,fast-math")
+#pragma GCC target ("sse4")
+#pragma comment(linker, "/stack:200000000")
 
 #include <bits/stdc++.h>
 #include <ext/pb_ds/tree_policy.hpp>
 #include <ext/pb_ds/assoc_container.hpp>
 
 #define deb(x) cout << #x << " is " << x << "\n";
-const double PI = 2 * acos(0.0);
 
 using namespace std;
 using namespace __gnu_pbds;
 
+const double PI = 2 * acos(0.0);
 template <typename T>
 using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
