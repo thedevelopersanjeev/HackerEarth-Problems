7d0f8e8e12878c82125dc2a3876d94fea2102d9d
Sanjeev Sharma <thedevelopersanjeev@gmail.com>
2020-01-25 14:50:30 +0530

    cleanup

--- main.cpp
+++ main.cpp
@@ -10,17 +10,10 @@
 #pragma GCC target ("sse4")
 
 #include <bits/stdc++.h>
-#include <ext/pb_ds/tree_policy.hpp>
-#include <ext/pb_ds/assoc_container.hpp>
 
 #define deb(x) cout << #x << " is " << x << "\n";
 
 using namespace std;
-using namespace __gnu_pbds;
-
-const double PI = 2 * acos(0.0);
-template <typename T>
-using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 int main(){
     ios_base::sync_with_stdio(false);
