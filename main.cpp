4813dd86a8df1bf05d07f01063818fb63cc74b32
Sanjeev Sharma <thedevelopersanjeev@gmail.com>
2020-01-26 18:27:31 +0530

    updated template

--- main.cpp
+++ main.cpp
@@ -3,17 +3,10 @@
 *   Author    :    Sanjeev Sharma                   *
 *   Email     :    thedevelopersanjeev@gmail.com    *
 *****************************************************/
-#pragma GCC optimize ("O3")
-#pragma GCC optimize("Ofast")
-#pragma GCC optimize ("unroll-loops")
-#pragma GCC optimize("no-stack-protector,fast-math")
-#pragma GCC target ("sse4")
-
 #include <bits/stdc++.h>
+using namespace std;
 
 #define deb(x) cout << #x << " is " << x << "\n";
-
-using namespace std;
 
 int main(){
     ios_base::sync_with_stdio(false);
