9e5280a0d0f395a24c49148b79965cd7baabb115
Sanjeev Sharma <thedevelopersanjeev@gmail.com>
2020-03-12 21:09:46 +0530

    template update

--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
-/****************************************************
-*   Template for coding contests                    *
-*   Author    :    Sanjeev Sharma                   *
-*   Email     :    thedevelopersanjeev@gmail.com    *
-*****************************************************/
-#include <bits/stdc++.h>
-using namespace std;
-
+/**
+ *	Author    :    Sanjeev Sharma                   
+ *	Email     :    thedevelopersanjeev@gmail.com  
+**/
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
 #define deb(x) cout << #x << " is " << x << "\n";
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
 
 int main(){
-    ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     
