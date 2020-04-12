255ecd92e34098276a3179140da427a1645e2099
Sanjeev Sharma <thedevelopersanjeev@gmail.com>
2020-04-12 21:57:27 +0530

    added IB puzzles part

---
+++ AtCoder/Contest 162/B.cpp
@@ -0,0 +1,42 @@
+/****************************************************
+*   Template for coding contests                    *
+*   Author    :    Sanjeev Sharma                   *
+*   Email     :    thedevelopersanjeev@gmail.com    *
+*****************************************************/
+#pragma GCC optimize ("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#pragma GCC optimize("no-stack-protector,fast-math")
+#pragma GCC target ("sse4")
+#pragma comment(linker, "/stack:200000000")
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#define deb(x) cout << #x << " is " << x << "\n";
+#define int long long
+#define mod 1000000007
+
+const double PI = 2 * acos(0.0);
+const long long INF = 1e18L + 5;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int32_t main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
+	int ans = 0;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		if (i % 3 != 0 && i % 5 != 0) {
+			ans += i;
+		}
+	}
+	cout << ans << "\n";
+	return 0;
+}
---
+++ Interviewbit/Strings/CompareVersionNumbers.cpp
@@ -0,0 +1,64 @@
+__int128_t atoint128_t(std::string const & in) {
+	__int128_t res = 0;
+	size_t i = 0;
+	bool sign = false;
+	if (in[i] == '-') {
+		++i;
+		sign = true;
+	}
+	if (in[i] == '+') {
+		++i;
+	}
+	for (; i < in.size(); ++i) {
+		const char c = in[i];
+		if (not std::isdigit(c))
+			throw std::runtime_error(std::string("Non-numeric character: ") + c);
+		res *= 10;
+		res += c - '0';
+	}
+	if (sign) {
+		res *= -1;
+	}
+	return res;
+}
+
+
+int Solution::compareVersion(string version1, string version2) {
+
+	for (auto &ch : version1) {
+		if (ch == '.') {
+			ch = ' ';
+		}
+	}
+
+	for (auto &ch : version2) {
+		if (ch == '.') {
+			ch = ' ';
+		}
+	}
+
+	istringstream s1(version1), s2(version2);
+
+	while (1) {
+		string n1, n2;
+		if (not(s1 >> n1)) {
+			n1 = "0";
+		}
+		if (not(s2 >> n2)) {
+			n2 = "0";
+		}
+		if (not s1 and not s2) {
+			return 0;
+		}
+
+		__int128_t x = atoint128_t(n1), y = atoint128_t(n2);
+
+		if (x < y) {
+			return -1;
+		}
+		if (x > y) {
+			return 1;
+		}
+	}
+
+}
