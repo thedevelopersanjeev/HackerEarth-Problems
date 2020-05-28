f1402a06ff72f9289014319d02a5428ec69dd28c
Sanjeev <thedevelopersanjeev@gmail.com>
2020-05-28 23:36:37 +0530

    added new questions

---
+++ Interviewbit/Tree Data Structure/BinaryTreeFromInorderAndPostorder.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+vector<int> inorder, postorder;
+unordered_map<int, int> mp;
+int i;
+
+TreeNode* buildTreeUtil(int s, int e) {
+	if (s > e)
+		return NULL;
+	int curr = postorder[i];
+	i--;
+	TreeNode* root = new TreeNode(curr);
+	if (s == e)
+		return root;
+	int index = mp[curr];
+	root->right = buildTreeUtil(index + 1, e);
+	root->left = buildTreeUtil(s, index - 1);
+	return root;
+}
+
+TreeNode* Solution::buildTree(vector<int> &A, vector<int> &B) {
+	inorder = A;
+	postorder = B;
+	for (int i = 0; i < inorder.size(); i++)
+		mp[inorder[i]] = i;
+	i = postorder.size() - 1;
+	return buildTreeUtil(0, inorder.size() - 1);
+}
---
+++ Interviewbit/Tree Data Structure/BinaryTreeFromInorderAndPreorder.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+vector<int> preorder, inorder;
+unordered_map<int, int> mp;
+int i;
+
+TreeNode* buildTreeUtil(int s, int e) {
+	if (s > e)
+		return NULL;
+	int curr = preorder[i];
+	i++;
+	TreeNode* root = new TreeNode(curr);
+	if (s == e)
+		return root;
+	int index = mp[curr];
+	root->left = buildTreeUtil(s, index - 1);
+	root->right = buildTreeUtil(index + 1, e);
+	return root;
+}
+
+TreeNode* Solution::buildTree(vector<int> &A, vector<int> &B) {
+	preorder = A;
+	inorder = B;
+	for (int i = 0; i < inorder.size(); i++)
+		mp[inorder[i]] = i;
+	i = 0;
+	return buildTreeUtil(0, preorder.size() - 1);
+}
