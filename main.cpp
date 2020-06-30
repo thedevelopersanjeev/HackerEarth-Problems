cd0f3d5cf86a8411c89f6a2350894940d31b7c62
Sanjeev <thedevelopersanjeev@gmail.com>
2020-07-01 00:16:37 +0530

    added new questions

--- main.cpp
+++ main.cpp
@@ -29,33 +29,94 @@ using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statisti
 
 template<class... Args>
 auto create(size_t n, Args&&... args) {
-	if constexpr(sizeof...(args) == 1)
-		return vector(n, args...);
-	else
-		return vector(n, create(args...));
+    if constexpr(sizeof...(args) == 1)
+        return vector(n, args...);
+    else
+        return vector(n, create(args...));
 }
 
 template<typename... T>
 void read(T&... args) {
-	((cin >> args), ...);
+    ((cin >> args), ...);
 }
 
 template<typename... T>
 void write(T&&... args) {
-	((cout << args), ...);
+    ((cout << args), ...);
+}
+
+void count_sort(vector<int> &p, vector<int> &c) {
+    int n = p.size();
+    vector<int> cnt(n);
+    for(auto x : c) {
+        cnt[x]++;
+    }
+    vector<int> p_new(n);
+    vector<int> pos(n);
+    pos[0] = 0;
+    for(int i = 1; i < n; i++) {
+        pos[i] = pos[i - 1] + cnt[i - 1];
+    }
+    for(auto x : p) {
+        int i = c[x];
+        p_new[pos[i]] = x;
+        pos[i]++;
+    }
+    p = p_new;
 }
 
 void solve() {
-
+    string s;
+    int n;
+    read(s);
+    s += "$";
+    n = s.size();
+    vector<int> p(n), c(n);
+    vector<pair<char, int>> a(n);
+    for (int i = 0; i < n; i++)
+        a[i] = {s[i], i};
+    sort(a.begin(), a.end());
+    for (int i = 0; i < n; i++)
+        p[i] = a[i].second;
+    c[p[0]] = 0;
+    for (int i = 1; i < n; i++) {
+        if (a[i].first == a[i - 1].first) {
+            c[p[i]] = c[p[i - 1]];
+        } else {
+            c[p[i]] = c[p[i - 1]] + 1;
+        }
+    }
+    int k = 0;
+    while ((1 << k) < n) {
+        for(int i = 0; i < n; i++) {
+            p[i] = (p[i] - (1 << k) + n) % n;
+        }
+        count_sort(p, c);
+        vector<int> c_new(n);
+        c_new[p[0]] = 0;
+        for(int i = 1; i < n; i++) {
+            pair<int, int> prev = {c[p[i - 1]], c[(p[i - 1] + (1 << k)) % n]};
+            pair<int, int> now = {c[p[i]], c[(p[i] + (1 << k)) % n]};
+            if(now == prev) {
+                c_new[p[i]] = c_new[p[i - 1]];
+            } else {
+                c_new[p[i]] = c_new[p[i - 1]] + 1;
+            }
+        }
+        c = c_new;
+        k++;
+    }
+    for (auto ele : p)
+        write(ele, " ");
 }
 
 int32_t main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 #ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
 #endif
-	solve();
-	return 0;
+    solve();
+    return 0;
 }
