bba35cc3c3c20d6bf6af2813dd0aaa857eb23551
Sanjeev <thedevelopersanjeev@gmail.com>
2020-07-05 13:00:32 +0530

    template update

--- main.cpp
+++ main.cpp
@@ -9,7 +9,6 @@
 #pragma GCC optimize("no-stack-protector")
 #pragma GCC optimize("fast-math")
 #pragma GCC target ("sse4")
-#pragma comment(linker, "/stack:200000000")
 
 #include <bits/stdc++.h>
 #include <ext/pb_ds/tree_policy.hpp>
@@ -18,7 +17,7 @@
 using namespace __gnu_pbds;
 using namespace std;
 
-#define deb(x) cout << #x << " is " << x << "\n";
+#define deb(x) cout << #x << " is " << x << "\n"
 #define int long long
 #define mod 1000000007
 
@@ -45,74 +44,13 @@ void write(T&&... args) {
     ((cout << args), ...);
 }
 
-void count_sort(vector<int> &p, vector<int> &c) {
-    int n = p.size();
-    vector<int> cnt(n);
-    for(auto x : c) {
-        cnt[x]++;
-    }
-    vector<int> p_new(n);
-    vector<int> pos(n);
-    pos[0] = 0;
-    for(int i = 1; i < n; i++) {
-        pos[i] = pos[i - 1] + cnt[i - 1];
-    }
-    for(auto x : p) {
-        int i = c[x];
-        p_new[pos[i]] = x;
-        pos[i]++;
-    }
-    p = p_new;
-}
-
 void solve() {
-    string s;
-    int n;
-    read(s);
-    s += "$";
-    n = s.size();
-    vector<int> p(n), c(n);
-    vector<pair<char, int>> a(n);
-    for (int i = 0; i < n; i++)
-        a[i] = {s[i], i};
-    sort(a.begin(), a.end());
-    for (int i = 0; i < n; i++)
-        p[i] = a[i].second;
-    c[p[0]] = 0;
-    for (int i = 1; i < n; i++) {
-        if (a[i].first == a[i - 1].first) {
-            c[p[i]] = c[p[i - 1]];
-        } else {
-            c[p[i]] = c[p[i - 1]] + 1;
-        }
-    }
-    int k = 0;
-    while ((1 << k) < n) {
-        for(int i = 0; i < n; i++) {
-            p[i] = (p[i] - (1 << k) + n) % n;
-        }
-        count_sort(p, c);
-        vector<int> c_new(n);
-        c_new[p[0]] = 0;
-        for(int i = 1; i < n; i++) {
-            pair<int, int> prev = {c[p[i - 1]], c[(p[i - 1] + (1 << k)) % n]};
-            pair<int, int> now = {c[p[i]], c[(p[i] + (1 << k)) % n]};
-            if(now == prev) {
-                c_new[p[i]] = c_new[p[i - 1]];
-            } else {
-                c_new[p[i]] = c_new[p[i - 1]] + 1;
-            }
-        }
-        c = c_new;
-        k++;
-    }
-    for (auto ele : p)
-        write(ele, " ");
+
 }
 
 int32_t main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    cin.tie(nullptr);
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
