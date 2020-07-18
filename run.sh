9aefa69ce3b38e78b69597a34734d789bbc21a91
Sanjeev <thedevelopersanjeev@gmail.com>
2020-07-18 21:03:21 +0530

    added new questions

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-g++ -std=c++17 -Wall -Wextra -Wno-sign-conversion -Wshadow -g -ggdb3 -D_GLIBCXX_ASSERTIONS -DDEBUG main.cpp -o main && ./main
+g++ -std=c++17 -Wshadow -Wall -g -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG main.cpp -o main && ./main
