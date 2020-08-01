f8da762da644797d4442cf060fe922593112c60b
Sanjeev <thedevelopersanjeev@gmail.com>
2020-08-01 18:48:33 +0530

    added new questions

--- Codeforces/600/B.cpp
+++ Codeforces/600/B.cpp
@@ -21,40 +21,20 @@ using namespace __gnu_pbds;
 template <typename T>
 using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
-int dp[51][51][51];
-
-int f(int n, int m, int k)
-{
-    if (dp[n][m][k] || (n * m) == k || k == 0)
-    {
-        return dp[n][m][k];
-    }
-    int ans = 1e9;
-    for (int i = 1; i <= n - i; i++)
-    {
-        for (int j = 0; j <= k; j++)
-        {
-            ans = min(ans, (m * m) + f(i, m, k - j) + f(n - i, m, j));
-        }
-    }
-    for (int i = 1; i <= m - i; i++)
-    {
-        for (int j = 0; j <= k; j++)
-        {
-            ans = min(ans, (n * n) + f(n, m - i, k - j) + f(n, i, j));
-        }
-    }
-    return dp[n][m][k] = ans;
-}
-
 void solve()
 {
-    int t, n, m, k;
-    cin >> t;
-    while (t--)
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n), b(m);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < m; i++)
+        cin >> b[i];
+    sort(a.begin(), a.end());
+    for (const auto &ele : b)
     {
-        cin >> n >> m >> k;
-        cout << f(n, m, k) << "\n";
+        auto it = upper_bound(a.begin(), a.end(), ele);
+        cout << it - a.begin() << " ";
     }
 }
 
