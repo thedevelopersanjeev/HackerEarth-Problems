d3c7032c2bc7d951fb378d366682cab1e8b105ef
Sanjeev <thedevelopersanjeev@gmail.com>
2020-08-02 16:29:19 +0530

    minor changes

--- main.cpp
+++ main.cpp
@@ -29,10 +29,10 @@ int32_t main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
-	// #ifndef ONLINE_JUDGE
-	// 	freopen("input.txt", "r", stdin);
-	// 	freopen("output.txt", "w", stdout);
-	// #endif
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
 	solve();
 	return 0;
 }
