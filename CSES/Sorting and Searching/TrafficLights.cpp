b77861d9fd32317347cec752a4676dcccab205cb
Sanjeev <thedevelopersanjeev@gmail.com>
2020-08-02 20:38:34 +0530

    added new questions

---
+++ CSES/Sorting and Searching/TrafficLights.cpp
@@ -0,0 +1,54 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("no-stack-protector")
+#pragma GCC optimize("fast-math")
+#pragma GCC optimize("trapv")
+#pragma GCC target("sse4")
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define deb(x) cout << #x << " is " << x << "\n"
+#define int long long
+#define mod 1000000007LL
+#define PI acosl(-1)
+
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void solve()
+{
+    int x, n;
+    cin >> x >> n;
+    set<int> points = {0, x};
+    multiset<int> lengths = {x};
+    while (n--)
+    {
+        cin >> x;
+        auto it = points.upper_bound(x);
+        int left = *prev(it);
+        int right = *it;
+        lengths.erase(lengths.find(right - left));
+        lengths.insert(x - left);
+        lengths.insert(right - x);
+        points.insert(it, x);
+        cout << *lengths.rbegin() << " ";
+    }
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // #ifndef ONLINE_JUDGE
+    // 	freopen("input.txt", "r", stdin);
+    // 	freopen("output.txt", "w", stdout);
+    // #endif
+    solve();
+    return 0;
+}
