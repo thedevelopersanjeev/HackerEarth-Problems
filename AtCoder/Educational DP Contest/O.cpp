65af33601f30ae9c9651c578a029aae70cd1eede
Sanjeev <thedevelopersanjeev@gmail.com>
2020-08-03 17:21:18 +0530

    added new questions

---
+++ AtCoder/Educational DP Contest/O.cpp
@@ -0,0 +1,63 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("no-stack-protector")
+#pragma GCC optimize("fast-math")
+#pragma GCC optimize("trapv")
+#pragma GCC target("sse4")
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define deb(x) cout << #x << " is " << x << "\n"
+#define int long long
+#define mod 1000000007LL
+#define PI acosl(-1)
+
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<vector<int>> arr(n, vector<int>(n));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    vector<int> dp(1LL << n);
+    dp[0] = 1;
+    for (int mask = 0; mask < (1LL << n) - 1; mask++)
+    {
+        int a = __builtin_popcount(mask);
+        for (int b = 0; b < n; b++)
+        {
+            if (arr[a][b] == 1 && (mask & (1LL << b)) == 0)
+            {
+                int newmask = mask ^ (1LL << b);
+                dp[newmask] = (dp[newmask] + dp[mask]) % mod;
+            }
+        }
+    }
+    cout << dp[(1LL << n) - 1];
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    solve();
+    return 0;
+}
---
+++ AtCoder/Educational DP Contest/P.cpp
@@ -0,0 +1,67 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("no-stack-protector")
+#pragma GCC optimize("fast-math")
+#pragma GCC optimize("trapv")
+#pragma GCC target("sse4")
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define deb(x) cout << #x << " is " << x << "\n"
+#define int long long
+#define mod 1000000007LL
+#define PI acosl(-1)
+
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+pair<int, int> dfs(vector<int> adj[], int a, int b)
+{
+    int white = 1, black = 0;
+    for (const auto &u : adj[a])
+    {
+        if (u != b)
+        {
+            auto curr = dfs(adj, u, a);
+            int temp = white;
+            white = (white * curr.second) % mod;
+            black = ((temp * curr.first) % mod + (black * (curr.first + curr.second)) % mod) % mod;
+        }
+    }
+    return {white, white + black};
+}
+
+void solve()
+{
+    int n, x, y;
+    cin >> n;
+    vector<int> adj[n];
+    for (int i = 0; i < n - 1; i++)
+    {
+        cin >> x >> y;
+        x--;
+        y--;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+    auto p = dfs(adj, 0, 0);
+    cout << (p.first + p.second) % mod;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    solve();
+    return 0;
+}
