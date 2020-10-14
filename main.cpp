3ff14b88f1ede3a5ccf30137823014db76e0c077
Sanjeev <thedevelopersanjeev@gmail.com>
2020-10-14 20:04:21 +0530

    updated template

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@
 #pragma GCC target("sse4")
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
 #define deb(x) cout << #x << " is " << x << "\n"
@@ -18,51 +17,46 @@ using namespace std;
 // C(n, r) = n! / (r! * (n - r)!)
 // C(n, r) = C(n, n - r)
 // C(n, r) = C(n - 1, r - 1) + C(n - 1, r)
-int nCr (int n, int r) {
-	long double res = 1;
-	r = min (r, n - r);
-
-	for (int i = 1; i <= r; i++) {
-		res = res * (n - r + i) / i;
-	}
-
-	return (int) (res + 0.01);
+int nCr(int n, int r) {
+    long double res = 1;
+    r = min(r, n - r);
+    for (int i = 1; i <= r; i++) {
+        res = res * (n - r + i) / i;
+    }
+    return (int)(res + 0.01);
 }
 
 // (x ^ y) % mod
-int modpow (int x, int y, int m) {
-	x %= m;
-	int res = 1LL;
-
-	while (y > 0) {
-		if (y & 1) {
-			res = (res * x) % m;
-		}
-
-		x = (x * x) % m;
-		y >>= 1;
-	}
-
-	return res;
+int modpow(int x, int y, int m) {
+    x %= m;
+    int res = 1LL;
+    while (y > 0) {
+        if (y & 1) {
+            res = (res * x) % m;
+        }
+        x = (x * x) % m;
+        y >>= 1;
+    }
+    return res;
 }
 
 void solve() {
-
+    int n;
+    cin >> n;
+    deb(n);
 }
 
 int32_t main() {
-	ios_base::sync_with_stdio (false);
-	cin.tie (nullptr);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
 #ifndef ONLINE_JUDGE
-	freopen ("input.txt", "r", stdin);
-	freopen ("output.txt", "w", stdout);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
 #endif
-	int tc = 1;
-	// cin >> tc;
-
-	while (tc--) {
-		solve();
-	}
-
-	return 0;
+    int tc = 1;
+    cin >> tc;
+    while (tc--) {
+        solve();
+    }
+    return 0;
 }
