d3c9a47d2c0b0d1eb8ffb1c5aba9b2a9236df775
Sanjeev <thedevelopersanjeev@gmail.com>
2020-10-24 18:09:37 +0530

    template update

--- main.cpp
+++ main.cpp
@@ -1,12 +1,8 @@
 #pragma GCC optimize("O3")
-#pragma GCC optimize("Ofast")
-#pragma GCC optimize("unroll-loops")
-#pragma GCC optimize("no-stack-protector")
-#pragma GCC optimize("fast-math")
-#pragma GCC optimize("trapv")
 #pragma GCC target("sse4")
 
 #include <bits/stdc++.h>
+
 using namespace std;
 
 #define deb(x) cout << #x << " is " << x << "\n"
@@ -14,54 +10,24 @@ using namespace std;
 #define mod 1000000007
 #define PI acos(-1)
 
-// C(n, r) = n! / (r! * (n - r)!)
-// C(n, r) = C(n, n - r)
-// C(n, r) = C(n - 1, r - 1) + C(n - 1, r)
-int nCr(int n, int r) {
-    long double res = 1;
-    r = min(r, n - r);
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
 
-    for (int i = 1; i <= r; i++) {
-        res = res * (n - r + i) / i;
-    }
+template <typename T>
+using max_heap = priority_queue<T>;
 
-    return (int)(res + 0.01);
-}
-
-// (x ^ y) % mod
-int modpow(int x, int y, int m) {
-    x %= m;
-    int res = 1LL;
-
-    while (y > 0) {
-        if (y & 1) {
-            res = (res * x) % m;
-        }
-
-        x = (x * x) % m;
-        y >>= 1;
-    }
-
-    return res;
-}
-
-void solve() {
+void solve(int tc) {
 }
 
 int32_t main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    // #ifndef ONLINE_JUDGE
-    //     freopen("input.txt", "r", stdin);
-    //     freopen("output.txt", "w", stdout);
-    // #endif
+    cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
     int tc = 1;
-
-    // cin >> tc;
-
-    while (tc--) {
-        solve();
-    }
-
+    cin >> tc;
+    for (int curr = 1; curr <= tc; curr++) solve(curr);
     return 0;
 }
