db46f3aac9557622ff045b051a953a17b625a8a9
Sanjeev <thedevelopersanjeev@gmail.com>
2020-10-24 22:31:11 +0530

    minor updates

--- main.cpp
+++ main.cpp
@@ -22,12 +22,12 @@ void solve(int tc) {
 int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    // #ifndef ONLINE_JUDGE
-    //     freopen("input.txt", "r", stdin);
-    //     freopen("output.txt", "w", stdout);
-    // #endif
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
     int tc = 1;
-    // cin >> tc;
+    cin >> tc;
     for (int curr = 1; curr <= tc; curr++) solve(curr);
     return 0;
 }
