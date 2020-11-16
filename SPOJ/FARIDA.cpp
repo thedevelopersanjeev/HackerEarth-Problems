0cc5f129ebe4594e19855fe203f2da5b0bbe1734
Sanjeev <thedevelopersanjeev@gmail.com>
2020-11-16 20:35:09 +0530

    added some more questions

---
+++ SPOJ/FARIDA.cpp
@@ -0,0 +1,69 @@
+#pragma GCC optimize("O3")
+#pragma GCC target("sse4")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define deb(x) cout << #x << " is " << x << "\n"
+#define int long long
+#define mod 1000000007
+#define PI acos(-1)
+
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+template <typename T>
+using max_heap = priority_queue<T>;
+
+template <typename... T>
+void read(T &... args) {
+    ((cin >> args), ...);
+}
+
+template <typename... T>
+void write(T &&... args) {
+    ((cout << args), ...);
+}
+
+template <typename T>
+void readContainer(T &t) {
+    for (auto &e : t) read(e);
+}
+
+template <typename T>
+void writeContainer(T &t) {
+    for (const auto &e : t) write(e, " ");
+    write("\n");
+}
+
+void solve(int tc) {
+    int n;
+    read(n);
+    if (n == 0) {
+        write("Case ", tc, ": ", 0, "\n");
+        return;
+    }
+    vector<int> arr(n);
+    readContainer(arr);
+    vector<int> dp(n, 0);
+    dp[0] = arr[0];
+    dp[1] = max(arr[0], arr[1]);
+    for (int i = 2; i < n; i++) {
+        dp[i] = max(dp[i - 2] + arr[i], dp[i - 1]);
+    }
+    write("Case ", tc, ": ", dp.back(), "\n");
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int tc = 1;
+    read(tc);
+    for (int curr = 1; curr <= tc; curr++) solve(curr);
+    return 0;
+}
