869cfe27a700bb70694c7b90a45e70cfdd9484ba
Sanjeev <thedevelopersanjeev@gmail.com>
2020-11-22 17:55:15 +0530

    added new questions

---
+++ Codeforces/978A.cpp
@@ -0,0 +1,68 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("no-stack-protector")
+#pragma GCC optimize("fast-math")
+#pragma GCC target("sse4")
+
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define deb(x) cout << #x << " is " << x << "\n"
+#define int long long
+#define mod 1000000007
+
+const double PI = 2 * acos(0.0);
+const long long INF = 1e18L + 5;
+
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename T>
+inline istream &operator>>(istream &in, vector<T> &a) {
+    for (auto &x : a)
+        in >> x;
+    return in;
+}
+
+template <typename T>
+inline ostream &operator<<(ostream &out, vector<T> &a) {
+    for (auto &x : a)
+        out << x << " ";
+    return out;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    cin >> arr;
+    unordered_map<int, int> mp;
+    vector<int> ans;
+    for (int i = n - 1; i >= 0; i--) {
+        if (mp[arr[i]] > 0)
+            continue;
+        mp[arr[i]] = 1;
+        ans.push_back(arr[i]);
+    }
+    reverse(ans.begin(), ans.end());
+    cout << ans.size() << "\n";
+    for (const auto &ele : ans)
+        cout << ele << " ";
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    solve();
+    return 0;
+}
