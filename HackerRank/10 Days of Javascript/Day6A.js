ad6f718490eb800783282a3136ff9f82f5781132
Sanjeev <thedevelopersanjeev@gmail.com>
2020-12-05 23:17:41 +0530

    added new questions

---
+++ HackerRank/10 Days of Javascript/Day6A.js
@@ -0,0 +1,47 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function getMaxLessThanK(n, k) {
+    var ans = 0;
+    for (var i = 1; i <= n; i++) {
+        for (var j = i + 1; j <= n; j++) {
+            var curr = i & j;
+            if (curr < k) {
+                ans = Math.max(ans, curr);
+            }
+        }
+    }
+    return ans;
+}
+
+
+function main() {
+    const q = +(readLine());
+
+    for (let i = 0; i < q; i++) {
+        const [n, k] = readLine().split(' ').map(Number);
+
+        console.log(getMaxLessThanK(n, k));
+    }
+}
---
+++ HackerRank/10 Days of Javascript/Day6B.js
@@ -0,0 +1,48 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+function getDayName(dateString) {
+    var d = new Date(dateString);
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+    return weekday[d.getDay()];
+}
+
+
+function main() {
+    const d = +(readLine());
+
+    for (let i = 0; i < d; i++) {
+        const date = readLine();
+
+        console.log(getDayName(date));
+    }
+}
