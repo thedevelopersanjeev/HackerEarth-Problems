3fc5642c0ab436dfcafdff830f65f662a733d474
Sanjeev <thedevelopersanjeev@gmail.com>
2020-12-19 19:37:41 +0530

    added new atcoder contest

---
+++ AtCoder/Beginner Contests/Contest 186/D.cpp
@@ -0,0 +1,67 @@
+#pragma GCC optimize("O3")
+#pragma GCC target("sse4")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define deb(x) cout << #x << " is " << x << "\n"
+#define int long long
+#define mod 1000000007
+#define PI acos(-1)
+
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+template <typename T>
+using max_heap = priority_queue<T>;
+
+template <typename... T>
+void read(T &... args) {
+    ((cin >> args), ...);
+}
+
+template <typename... T>
+void write(T &&... args) {
+    ((cout << args), ...);
+}
+
+template <typename T>
+void readContainer(T &t) {
+    for (auto &e : t) read(e);
+}
+
+template <typename T>
+void writeContainer(T &t) {
+    for (const auto &e : t) write(e, " ");
+
+    write("\n");
+}
+
+void solve(int tc) {
+    int n;
+    read(n);
+    vector<int> a(n);
+    readContainer(a);
+    sort(a.begin(), a.end());
+    int res = 0;
+    int sm = 0;
+
+    for (int i = 0; i < n; i++) {
+        res += (i * a[i] - sm);
+        sm += a[i];
+    }
+
+    write(res);
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc = 1;
+    // read(tc);
+
+    for (int curr = 1; curr <= tc; curr++) solve(curr);
+
+    return 0;
+}
