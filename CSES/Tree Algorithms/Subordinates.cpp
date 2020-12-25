d4e20b33acf4a53a9c54adc07b4518a368bfa52c
Sanjeev <thedevelopersanjeev@gmail.com>
2020-12-25 15:02:13 +0530

    added more questions

---
+++ CSES/Tree Algorithms/Subordinates.cpp
@@ -0,0 +1,99 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("no-stack-protector")
+#pragma GCC optimize("fast-math")
+#pragma GCC optimize("trapv")
+#pragma GCC target("sse4")
+
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define deb(x) cout << #x << " is " << x << "\n"
+#define int long long
+#define MOD 1000000007LL
+#define mod(x) (x % MOD + MOD) % MOD
+#define PI acos(-1)
+
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+template <typename T>
+using max_heap = priority_queue<T>;
+
+template <class T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename... T>
+void read(T &... args) {
+    ((cin >> args), ...);
+}
+
+template <typename... T>
+void write(T &&... args) {
+    ((cout << args), ...);
+}
+
+template <typename T>
+void readContainer(T &t) {
+    for (auto &e : t) read(e);
+}
+
+template <typename T>
+void writeContainer(T &t) {
+    for (const auto &e : t) write(e, " ");
+
+    write("\n");
+}
+
+void dfs(int child, vector<int> adj[], int parent, vector<int> &ans) {
+    int curr = 0;
+
+    for (const auto &v : adj[child]) {
+        if (v != parent) {
+            dfs(v, adj, child, ans);
+            curr += (1 + ans[v]);
+        }
+    }
+
+    ans[child] = curr;
+}
+
+void solve(int tc) {
+    int n, ele;
+    read(n);
+    vector<int> adj[n];
+
+    for (int i = 1; i < n; i++) {
+        read(ele);
+        ele--;
+        adj[i].push_back(ele);
+        adj[ele].push_back(i);
+    }
+
+    vector<int> ans(n, 0);
+
+    dfs(0, adj, -1, ans);
+
+    writeContainer(ans);
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int tc = 1;
+    // read(tc);
+
+    for (int curr = 1; curr <= tc; curr++) solve(curr);
+
+    return 0;
+}
