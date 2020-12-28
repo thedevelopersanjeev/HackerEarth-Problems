34301e261ec5c1efd6f107df0e8a837dd3b79279
Sanjeev <thedevelopersanjeev@gmail.com>
2020-12-28 21:17:12 +0530

    added run.sh

---
+++ run.sh
@@ -0,0 +1,2 @@
+g++ -std=c++17 -O2 -Wall -Wextra -pedantic -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -Wno-unused-result -Wno-sign-conversion -fsanitize=address -fsanitize=undefined -fsanitize=float-divide-by-zero -fsanitize=float-cast-overflow -fno-sanitize-recover=all -fstack-protector-all -D_FORTIFY_SOURCE=2 main.cpp
+./a.out < input.txt > output.txt
