bb9709fc9205b5bf90458a6605f988a58ac534f3
Sanjeev <thedevelopersanjeev@gmail.com>
2021-02-19 16:03:57 +0530

    added new questions

--- CodeChef/TRISQ.cpp
+++ CodeChef/TRISQ.cpp
@@ -1,16 +1,16 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	int t, b;
-	cin >>t;
-	while(t--){
-		cin >>b;
-		int x = b/2 - 1;
-		cout <<(x * (x+1)) / 2 <<"\n";
-	}
-	return 0;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t, b;
+    cin >> t;
+    while (t--) {
+        cin >> b;
+        int x = b / 2 - 1;
+        cout << (x * (x + 1)) / 2 << "\n";
+    }
+    return 0;
 }
